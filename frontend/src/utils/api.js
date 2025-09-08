a9ab59b62f89974c3cd64a23141d58315288a6d0
vannnnnn <134368005+BUTTERCUPXCZ@users.noreply.github.com>
2025-09-08 20:01:19 +0800

    changes gamay sa URI

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,5 +1,5 @@
 const config = {
-    apiUrl: import.meta.env.VITE_API_URL || (import.meta.env.MODE === 'production' ? 'https://your-backend-url.onrender.com' : 'http://localhost:3000')
+    apiUrl: import.meta.env.VITE_API_URL || 'http://localhost:3000'
 }
 
 export default config
